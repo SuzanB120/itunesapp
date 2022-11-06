be690031d28632e0a3ab4914000f6fdb31a36da8
suzan <badarnisuzan@gmail.com>
2022-11-06 13:20:50 +0200

    06112022

--- app/App.js
+++ app/App.js
@@ -1,12 +1,37 @@
 import React from "react";
-import {Viwe, Text,Stylesheet} from 'react-native';
+import {View,Text ,TouchableOpacity,
+ StyleSheet, Alert} from 'react-native';
 
 const App = () => {
+
+const doSomething = async() => {
+  const api = 'https://itunes.apple.com/search?term=metalica';
+  const response = await fetch(api, {
+    method:'get'
+  });
+
+  const data = await response.json();
+  console.log(data);
+}
+
   return(
-    <View>
-      <Text> hello suzan</Text>
+    <View style={myStyle.container}>
+      <TouchableOpacity style={myStyle.btn} onPress={doSomething}>
+        <Text style={myStyle.btntext}>Click me</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const myStyle = StyleSheet.create({
+  btn:{width:'50%', paddingVertical:14 ,
+ alignItems:'center', backgroundColor:'#17B890', 
+ borderRadius:14
+ }, 
+  btntext: { color:'#DEE5E5', fontsize:22, fontWeight:'700'},
+  container: { flex:1, 
+    alignItems:'center', 
+    justifContent:'center', backgroundColor:'#C71585'}
+});
 
 export default App;
