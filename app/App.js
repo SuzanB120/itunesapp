c102324be513256af15d465036b7655a1f3cfc5a
suzan <badarnisuzan@gmail.com>
2022-11-06 11:48:00 +0200

    06112022

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Viwe, Text,Stylesheet} from 'react-native';
+
+const App = () => {
+  return(
+    <View>
+      <Text> hello suzan</Text>
+    </View>
+  )
+}
+
+export default App;
